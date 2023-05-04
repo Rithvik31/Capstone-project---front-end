f1a0dddd11568d32524c052ac8a28cddd873c294
Rithvik31 <rithvikchinnu@gmail.com>
2023-05-04 22:45:28 +0530

    update

--- create-user/script.js
+++ create-user/script.js
@@ -1,12 +1,41 @@
-const form = document.querySelector('form');
+const form = document.getElementById('user-form');
 form.addEventListener('submit', createUser);
 
 function createUser(event) {
   event.preventDefault();
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+
+  const name = nameInput.value.trim();
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  // Basic client-side form validation
+  if (name === '') {
+    showError(nameInput, 'Name is required');
+    return;
+  }
+
+  if (email === '') {
+    showError(emailInput, 'Email is required');
+    return;
+  } else if (!isValidEmail(email)) {
+    showError(emailInput, 'Invalid email format');
+    return;
+  }
+
+  if (password === '') {
+    showError(passwordInput, 'Password is required');
+    return;
+  } else if (password.length < 8) {
+    showError(passwordInput, 'Password must be at least 8 characters');
+    return;
+  }
+
   const user = { name, email, password };
+
   axios.post('http://localhost:8080/users/register', user)
     .then(response => {
       console.log(response.data);
@@ -21,11 +50,26 @@ function createUser(event) {
 
 const passwordInput = document.getElementById('password');
 const signupBtn = document.getElementById('signup-btn');
+const passwordError = document.getElementById('password-error');
 
 passwordInput.addEventListener('input', function() {
-    if (passwordInput.value.length >= 8) {
-        signupBtn.disabled = false;
-    } else {
-        signupBtn.disabled = true;
-    }
+  const passwordValue = passwordInput.value.trim();
+
+  if (passwordValue.length >= 8) {
+    signupBtn.disabled = false;
+    passwordError.textContent = '';
+  } else {
+    signupBtn.disabled = true;
+    passwordError.textContent = 'Password must be at least 8 characters';
+  }
 });
+
+function showError(input, errorMessage) {
+  const errorElement = input.nextElementSibling;
+  errorElement.textContent = errorMessage;
+}
+
+function isValidEmail(email) {
+  const regex = /\S+@\S+\.\S+/;
+  return regex.test(email);
+}
