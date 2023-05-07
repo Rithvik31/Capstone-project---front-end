ef3f8508d59ff6c57b3ece1a3c68d70907c62b53
Rithvik31 <rithvikchinnu@gmail.com>
2023-05-07 11:53:25 +0530

    update

--- monefydashboard/app.js
+++ monefydashboard/app.js
@@ -13,3 +13,38 @@ axios.get('http://localhost:8080/finance/user-finances')
     }
   })
   .catch(error => console.error(error));
+
+  const incomeEndpoint = 'http://localhost:8080/finance/total-income';
+const expensesEndpoint = 'http://localhost:8080/finance/total-expenses';
+
+// Fetch data from endpoints using Axios
+axios.all([
+  axios.get(incomeEndpoint),
+  axios.get(expensesEndpoint)
+]).then(axios.spread((incomeResponse, expensesResponse) => {
+  const income = incomeResponse.data;
+  const expenses = expensesResponse.data;
+
+  // // Calculate the total income and expenses
+  // const totalIncome = income.reduce((acc, curr) => acc + curr, 0);
+  // const totalExpenses = expenses.reduce((acc, curr) => acc + curr, 0);
+
+  // Create the donut chart using Chart.js
+  const donutChart = new Chart('donut-chart', {
+    type: 'doughnut',
+    data: {
+      labels: ['Income', 'Expenses'],
+      datasets: [{
+        data: [income, expenses],
+        backgroundColor: ['#36A2EB', '#FF6384'],
+        borderWidth: 0
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  });
+})).catch(error => {
+  console.log('Error fetching data:', error);
+});
